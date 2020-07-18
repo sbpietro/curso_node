35ac3d519232fa9103ce2cc5d21ee4f322403a48
Stefano Bretas Di Pietro <stefanobretas@gmail.com>
2020-07-18 01:03:11 -0300

    Cria conexão com banco de dados MySQL

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,6 +1,12 @@
+var dbConnection = require('../../config/dbConnection');
+
 module.exports = function(app){
+
+    var conn = dbConnection();
     app.get('/noticias', function(req, res){
-        res.render("noticias/noticias");
+        conn.query('select * from noticias', function(error, result){
+            res.render('noticias/noticias', {noticias: result});
+        });
     });
 }
 
--- mod_teste.js
+++
@@ -1,4 +0,0 @@
-module.exports = function(){
-    var msg = "Teste";
-    return msg;
-}
